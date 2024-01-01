c36e902759bc00343d611ca5398ed00ed1de654e
Ömer <omersay6@gmail.com>
2024-01-01 18:35:50 +0400

    [ADD] sever item tooltip

--- src/components/atoms/server-list-bar-item/index.tsx
+++ src/components/atoms/server-list-bar-item/index.tsx
@@ -1,5 +1,6 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { motion } from "framer-motion";
+import { usePopper } from "react-popper";
 
 type Props = {
   index: number;
@@ -19,6 +20,20 @@ function ServerListBarItem(props: Props) {
     props.onClickItem && props.onClickItem(activeIndex);
   };
 
+  const [referenceElement, setReferenceElement] = useState<HTMLElement | null>(
+    null
+  );
+  const [popperElement, setPopperElement] = useState<HTMLElement | null>(null);
+  const [arrowElement, setArrowElement] = useState<HTMLElement | null>(null);
+  const { styles, attributes } = usePopper(referenceElement, popperElement, {
+    placement: "right",
+    strategy: "fixed",
+    modifiers: [
+      { name: "arrow", options: { element: arrowElement, padding: 10 } },
+      { name: "offset", options: { offset: [0, 20] } },
+    ],
+  });
+
   return (
     <div key={index} className="relative flex w-[72px] px-3">
       <motion.div
@@ -27,6 +42,7 @@ function ServerListBarItem(props: Props) {
         animate="open"
       />
       <motion.div
+        ref={setReferenceElement}
         onMouseOver={() => setIsHovered(true)}
         onMouseLeave={() => setIsHovered(false)}
         onClick={() => onClickItemHandler(index ?? 0)}
@@ -42,6 +58,17 @@ function ServerListBarItem(props: Props) {
           <span className="text-[10px]">OmrSy</span>
         </div>
       </motion.div>
+      {isHovered && (
+        <div ref={setPopperElement} style={{ ...styles.popper, zIndex: 1 }}>
+          <div
+            ref={setArrowElement}
+            className="w-3 h-3 !rotate-45 bg-black absolute left-0 top-1/2 -translate-y-1/2 -translate-x-1/3 z-0"
+          />
+          <div className="h-full bg-black max-w-[200px] relative z-10 text-white rounded-md p-2 text-sm">
+            omer-say-test-work
+          </div>
+        </div>
+      )}
     </div>
   );
 }
